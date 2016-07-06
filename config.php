abbb72c904535e75ad44944f12bf3ca6bb8abf7a
fanti001 <biuro@kamedia.pl>
2016-07-06 08:45:18 +0200

    przenosiny

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 date_default_timezone_set('Europe/Warsaw');
-$CONF_http = 'http://allegro.kamedia.pl';
+$CONF_http = 'http://188.116.32.113/allegro.fotolia';
 $CONF_sendEmailOnOrder = TRUE;
 ini_set('display_errors', 1);
-//error_reporting(E_ALL | E_STRICT);
+error_reporting(E_ALL | E_STRICT);
 
 $CONF_aukcje = array(
 	
@@ -50,6 +50,15 @@ $CONF_Fotolia_search_filters = array(
 	'content_type:all'          =>1,
 	'offensive:2'               =>1,
 );
+
+global $db;
+	class DB_Conf {
+		protected static $db_host = 'localhost';
+		protected static $db_user = 'kamedia';
+		protected static $db_pass = 'kamedia001';
+		protected static $db_base = 'kamedia';
+	}
+/*
 
 if ($_SERVER["HTTP_HOST"] == 'localhost') {
 	error_reporting(2047);
